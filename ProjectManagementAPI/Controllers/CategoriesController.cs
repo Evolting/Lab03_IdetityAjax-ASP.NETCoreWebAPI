2f328c0dfe33c9dce3f21abc37e7fcb7a0176df8
Long Nguyen <89681530+Evolting@users.noreply.github.com>
2023-03-21 23:14:28 +0700

    Add project files.

---
+++ ProjectManagementAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Repositories;
+
+namespace ProjectManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : Controller
+    {
+        private ICategoryRepository repository = new CategoryRepository();
+
+        // GET: api/Categories
+        [HttpGet]
+        public IActionResult GetCategories()
+        {
+            return Ok(repository.GetCategories());
+        }
+    }
+}
